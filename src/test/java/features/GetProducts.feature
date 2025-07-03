395063940861a4e9a81955963e91e81b5c26632e
Dinesh Borude <borudedinesh2@gmail.com>
2025-07-04 01:10:01 +0530

    response body

--- src/test/java/features/GetProducts.feature
+++ src/test/java/features/GetProducts.feature
@@ -5,3 +5,11 @@
       Given I hit the url of get products api
       When I pass the url of products in the request
       Then I recieve the response code as 200
+
+    Scenario: Verify the rate of the first product is correct
+      Given I hit the url of get products api
+      When I pass the url of products in the request
+      Then I verify that the rate of the first product is <Rate>
+      Example:
+        | FirstProductRate |
+        | 3.9              |
--- src/test/java/step_definations/Products.java
+++ src/test/java/step_definations/Products.java
@@ -4,7 +4,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 
 import static org.junit.Assert.assertEquals;
@@ -13,15 +15,15 @@ import static org.junit.Assert.assertEquals;
 public class Products {
 
     public int StatusCode;
-    RequestSpecification httpRequest;
-    Response response;
-    int ResponseCode;
+    public RequestSpecification httpRequest;
+    public Response response;
+    public int ResponseCode;
+    public ResponseBody body;
+
     @Given("I hit the url of get products api")
     public void i_hit_the_url_of_get_products_api() {
 
         RestAssured.baseURI = "https://fakestoreapi.com/";
-
-
 
     }
 
@@ -40,8 +42,19 @@ public class Products {
 
         assertEquals(ResponseCode,200);
 
-
     }
 
+    @Then("I verify that the rate of the first product is <Rate>")
+    public void i_verify_that_the_rate_of_the_first_product_is(String arg0) {
+
+        body = response.getBody();
+
+        String responseBody = body.asString();
+
+        JsonPath jsonpath = response.jsonPath();
+
+        String s = jsonpath.getJsonObject("rating").toString();
+
+    }
 
 }
